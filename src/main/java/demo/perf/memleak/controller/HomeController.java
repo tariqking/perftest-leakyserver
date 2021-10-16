e72d36fc8cb461b8cba6e00e4c52e21e0dc29e3b
Kaushal Dalvi <kaushaldalvi@gmail.com>
2021-10-16 13:38:52 -0400

    update memory leak

--- src/main/java/demo/perf/memleak/controller/HomeController.java
+++ src/main/java/demo/perf/memleak/controller/HomeController.java
@@ -7,7 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 public class HomeController {
@@ -15,13 +19,33 @@ public class HomeController {
     // Define the logger object for this class
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
+    public static String getResourceFileAsString(String fileName) {
+        InputStream is = getResourceFileAsInputStream(fileName);
+        if (is != null) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+        } else {
+            throw new RuntimeException("resource not found");
+        }
+    }
+
+    public static InputStream getResourceFileAsInputStream(String fileName) {
+        ClassLoader classLoader = HomeController.class.getClassLoader();
+        return classLoader.getResourceAsStream(fileName);
+    }
+
     @GetMapping(value = "/")
     public UUID loadHomePage(HttpSession session, Model model) {
         UUID uid = (UUID) session.getAttribute("uid");
         if (uid == null) {
             uid = UUID.randomUUID();
         }
-        final byte[] leaky = new byte[1024 * 1024 * 100];
+
+        String str = null;
+        str = getResourceFileAsString("large-file.txt");
+
+        str.intern();
+
         session.setAttribute("uid", uid);
         model.addAttribute("uid", uid);
         try {
