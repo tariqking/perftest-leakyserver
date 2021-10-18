c5eff4be66ec5d5fafb5cb97d0429e0ab8769610
Kaushal Dalvi <kaushaldalvi@gmail.com>
2021-10-17 21:58:24 -0400

    update memory leak rate

--- src/main/java/demo/perf/memleak/controller/HomeController.java
+++ src/main/java/demo/perf/memleak/controller/HomeController.java
@@ -57,7 +57,7 @@ public class HomeController {
     }
 
     @GetMapping(value = "/login")
-    public String login(HttpSession session, Model model) {
+    public String login() {
         return "Login successful";
     }
 
